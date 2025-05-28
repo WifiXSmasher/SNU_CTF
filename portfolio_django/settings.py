4406f6c518b0a33740cf1f1ad8012e52bf41cae0
WifiXSmasher <ruchirkalla007@gmail.com>
2025-05-28 16:47:00 +0530

    turned of debug

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True#turen off for vercel deployment
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
