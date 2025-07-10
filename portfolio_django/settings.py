894beb17dd9132734dc512238f2b13d262685b6a
Ruchir .U. Kalla <164368036+WifiXSmasher@users.noreply.github.com>
2025-07-10 14:22:53 +0530

    Update settings.py
    made the Debug False

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True#turen off for vercel deployment
-DEBUG=config('DEBUG')
+DEBUG=False
 
 
 ALLOWED_HOSTS = [
