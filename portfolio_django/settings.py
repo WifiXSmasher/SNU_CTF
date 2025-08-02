b797e21c2113bd727db7b989de3b49edf1e62073
Ruchir .U. Kalla <164368036+WifiXSmasher@users.noreply.github.com>
2025-08-02 22:40:15 +0530

    Update settings.py
    debug -false

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = False
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True#turen off for vercel deployment
-DEBUG=config('DEBUG')
-
+# DEBUG=config('DEBUG')
+DEBUG=False
 
 ALLOWED_HOSTS = [
     'localhost',
